9297225651c14b688fc33f77104d38a95a930a43
Akhmad Miftah Riyadhi <akhmadmiftah96@gmail.com>
2021-11-29 10:07:35 +0700

    If statement

--- main.go
+++ main.go
@@ -1,35 +1,60 @@
 package main
 
-import (
-	"fmt"
-	"log"
+import "fmt"
 
-	"main.go/simpleinterest"
-	// "learnpackage/simpleinterest"
-)
+// "learnpackage/simpleinterest"
 
-var p, r, t = -5000.0, 10.0, 1.0
+// var p, r, t = -5000.0, 10.0, 1.0
 
-func init() {
-	// fmt.Println("Simple interest package initialized")
-	println("Main package intitialized")
-	if p < 0 {
-		log.Fatal("Principal is less the zero")
-	}
-	if r < 0 {
-		log.Fatal("Rate of interest is less the zero")
-	}
-	if t < 0 {
-		log.Fatal("Duration is less the zero")
-	}
-}
+// func init() {
+// 	// fmt.Println("Simple interest package initialized")
+// 	println("Main package intitialized")
+// 	if p < 0 {
+// 		log.Fatal("Principal is less the zero")
+// 	}
+// 	if r < 0 {
+// 		log.Fatal("Rate of interest is less the zero")
+// 	}
+// 	if t < 0 {
+// 		log.Fatal("Duration is less the zero")
+// 	}
+// }
 
 func main() {
-	fmt.Println("Simple interest calculation")
+	// fmt.Println("Simple interest calculation")
 	// p := 5000.0
 	// r := 10.0
 	// t := 1.0
-	si := simpleinterest.Calculate(p, r, t)
+	// si := simpleinterest.Calculate(p, r, t)
 
-	fmt.Println("Simple interest is", si)
+	// fmt.Println("Simple interest is", si)
+
+	// If statement
+	// num := 102
+	// if num%2 == 0 {
+	// 	fmt.Println("The number", num, "is even")
+	// 	return
+	// }
+	// fmt.Println("The number", num, "is odd")
+	// } else {
+	// 	fmt.Println("The number", num, "is odd")
+	// }
+	// if num <= 50 {
+	// 	fmt.Println(num, "is less than or equal to 50")
+	// } else if num >= 51 && num <= 100 {
+	// 	fmt.Println(num, "is between 51 and 100")
+	// } else {
+	// 	fmt.Println(num, "is greater than 100")
+	// }
+	// if num := 10; num%2 == 0 {
+	// 	fmt.Println(num, "is even")
+	// } else {
+	// 	fmt.Println(num, "is odd")
+	// }
+	num := 10
+	if num%2 == 0 {
+		fmt.Println("The number is even")
+	} else {
+		fmt.Println("The number is odd")
+	}
 }
