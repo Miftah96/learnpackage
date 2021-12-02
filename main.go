67b41c44b998bb729b0d40142064b1772947c019
Akhmad Miftah Riyadhi <akhmadmiftah96@gmail.com>
2021-12-02 10:34:02 +0700

    Switch

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // "learnpackage/simpleinterest"
 
@@ -19,7 +22,10 @@ import "fmt"
 // 		log.Fatal("Duration is less the zero")
 // 	}
 // }
-
+func number() int {
+	num := 15 * 5
+	return num
+}
 func main() {
 	// fmt.Println("Simple interest calculation")
 	// p := 5000.0
@@ -100,7 +106,65 @@ func main() {
 	// 	fmt.Printf("%d * %d = %d\n", no, i, no*i)
 	// }
 
+	// for {
+	// 	fmt.Println("Hello World")
+	// }
+
+	// finger := 9
+	// fmt.Printf("Finger %d is ", finger)
+	// switch finger {
+	// case 1:
+	// 	fmt.Println("Thumb")
+	// case 2:
+	// 	fmt.Println("Index")
+	// case 3:
+	// 	fmt.Println("Middle")
+	// case 4:
+	// 	fmt.Println("Ring")
+	// case 5:
+	// 	fmt.Println("Pinky")
+	// default:
+	// 	fmt.Println("incorrect finger number")
+	// }
+
+	// letter := "A"
+	// fmt.Printf("Letter %s is a ", letter)
+	// switch letter {
+	// case "a", "e", "i", "o", "u":
+	// 	fmt.Println("vowel")
+	// default:
+	// 	fmt.Println(" not a vowel")
+	// }
+
+	// num := 75
+	// switch {
+	// case num >= 0 && num <= 50:
+	// 	fmt.Printf("%d is greather than 0 and less than 50", num)
+	// case num >= 51 && num <= 100:
+	// 	fmt.Printf("%d is greather than 51 and less than 100", num)
+	// case num >= 101:
+	// 	fmt.Printf("%d is greather than 100", num)
+	// }
+	// switch num := number(); {
+	// switch num := -25; {
+	// case num < 50:
+	// 	if num < 0 {
+	// 		break
+	// 	}
+	// 	fmt.Printf("%d is lesser than 50\n", num)
+	// 	fallthrough
+	// case num < 100:
+	// 	fmt.Printf("%d is lesser than 100\n", num)
+	// 	fallthrough
+	// case num < 200:
+	// 	fmt.Printf("%d is lesser than 200\n", num)
+	// }
+randloop:
 	for {
-		fmt.Println("Hello World")
+		switch i := rand.Intn(100); {
+		case i%2 == 0:
+			fmt.Printf("Generated even number %d", i)
+			break randloop
+		}
 	}
 }
