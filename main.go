317045e5856c0725a2d26dce0d1a7d087ebfa75a
Akhmad Miftah Riyadhi <akhmadmiftah96@gmail.com>
2021-12-09 10:05:13 +0700

    Array and slice

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 // "learnpackage/simpleinterest"
@@ -22,11 +21,79 @@ import (
 // 		log.Fatal("Duration is less the zero")
 // 	}
 // }
-func number() int {
-	num := 15 * 5
-	return num
+// func number() int {
+// 	num := 15 * 5
+// 	return num
+// }
+
+func changeLocal(num [5]int) {
+	num[0] = 55
+	fmt.Println("inside funtion", num)
 }
+
+func printarray(a [3][2]string) {
+	for _, v1 := range a {
+		for _, v2 := range v1 {
+			fmt.Printf("%s ", v2)
+		}
+		fmt.Printf("\n")
+	}
+}
+
 func main() {
+	cars := []string{"Ferrari", "Honda", "Ford"}
+	fmt.Println("cars:", cars, "has old length", len(cars), "and capacity", cap(cars))
+	cars = append(cars, "Toyota")
+	fmt.Println("cars:", cars, "has new length", len(cars), "and capacity", cap(cars))
+	// i := make([]int, 5, 5)
+	// fmt.Println(i)
+
+	// fruitarray := [...]string{"apple", "orange", "grape", "mango", "water melon", "pine apple", "chikoo"}
+	// fruitslice := fruitarray[1:3]
+	// fmt.Printf("length of slice %d capacity %d\n", len(fruitslice), cap(fruitslice))
+	// fruitslice = fruitslice[:cap(fruitslice)]
+	// fmt.Println("After re-slicing length is", len(fruitslice), "and capacity is", cap(fruitslice))
+
+	// numa := [3]int{78, 79, 80}
+	// nums1 := numa[:]
+	// nums2 := numa[:]
+	// fmt.Println("array before change 1", numa)
+	// nums1[0] = 100
+	// fmt.Println("array after modification to slice nums1", numa)
+	// nums2[1] = 101
+	// fmt.Println("array after modification to slice nums2", numa)
+
+	// darr := [...]int{57, 87, 90, 82, 100, 78, 67, 69, 59}
+	// dslice := darr[2:5]
+	// fmt.Println("array before", darr)
+	// for i := range dslice {
+	// 	dslice[i]++
+	// }
+	// fmt.Println("array after", darr)
+
+	// c := []int{6, 7, 8}
+	// fmt.Println(c)
+
+	// a := [5]int{76, 77, 78, 79, 80}
+	// var b []int = a[1:4]
+	// fmt.Println(b)
+
+	// a := [3][2]string{
+	// 	{"lion", "tiger"},
+	// 	{"cat", "dog"},
+	// 	{"piegon", "peacock"},
+	// }
+	// printarray(a)
+	// var b [3][2]string
+	// b[0][0] = "apple"
+	// b[0][1] = "samsung"
+	// b[1][0] = "microsoft"
+	// b[1][1] = "google"
+	// b[2][0] = "AT&T"
+	// b[2][1] = "T-Mobile"
+	// fmt.Printf("\n")
+	// printarray(b)
+
 	// fmt.Println("Simple interest calculation")
 	// p := 5000.0
 	// r := 10.0
@@ -159,12 +226,52 @@ func main() {
 	// case num < 200:
 	// 	fmt.Printf("%d is lesser than 200\n", num)
 	// }
-randloop:
-	for {
-		switch i := rand.Intn(100); {
-		case i%2 == 0:
-			fmt.Printf("Generated even number %d", i)
-			break randloop
-		}
-	}
+	// randloop:
+	// 	for {
+	// 		switch i := rand.Intn(100); {
+	// 		case i%2 == 0:
+	// 			fmt.Printf("Generated even number %d", i)
+	// 			break randloop
+	// 		}
+	// 	}
+
+	// var a [3]int
+	// a[0] = 12
+	// a[1] = 78
+	// a[2] = 60
+
+	// a := [3]int{12, 78, 50}
+	// a := [3]int{12, 78, 50, 30}
+
+	// a := [3]int{12}
+
+	// a := [...]int{12, 78, 50}
+	// a := [3]int{12, 78, 50}
+	// var b [5]int
+	// b = a
+	// fmt.Println(a)
+
+	// a := [...]string{"USA", "Chine", "India", "Germany", "France"}
+	// b := a
+	// b[0] = "Singapore"
+	// fmt.Println("a is ", a)
+	// fmt.Println("b is ", b)
+
+	// num := [...]int{5, 6, 7, 8, 8}
+	// fmt.Println("before passing to function", num)
+	// changeLocal(num)
+	// fmt.Println("After passing to function", num)
+
+	// a := [...]float64{67.7, 89.8, 21, 78}
+	// fmt.Println("length of a is", len(a))
+	// for i := 0; i < len(a); i++ {
+	// 	fmt.Printf("%d th element of a is %.2f\n", i, a[i])
+	// }
+	// sum := float64(0)
+	// for i, v := range a {
+	// for _, v := range a {
+	// 	fmt.Printf("%d  the element of  a is %.2f\n", i, v)
+	// 	sum += v
+	// }
+	// fmt.Println("\nsum of all elements of a", sum)
 }
